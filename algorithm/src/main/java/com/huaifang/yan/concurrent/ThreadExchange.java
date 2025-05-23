8bf7b5ec546cc223ba2a0c6d2b35f38149d5bf76
huaifang.yan <huaifang.yan@ly.com>
2025-05-23 23:43:21 +0800

    lua 脚本限制流量 锁的实现

--- algorithm/src/main/java/com/huaifang/yan/concurrent/ThreadExchange.java
+++ algorithm/src/main/java/com/huaifang/yan/concurrent/ThreadExchange.java
@@ -10,28 +10,21 @@ public class ThreadExchange {
 
     private static final Executor priorityExecutor=new ThreadPoolExecutor(1,1,3, TimeUnit.SECONDS, new PriorityBlockingQueue<>(10),new ConsumerThreadFactory("JOB"),new ThreadPoolExecutor.AbortPolicy());
 
-   private static final ExecutorService exchangeExecutor=new ThreadPoolExecutor(0,5,3, TimeUnit.SECONDS, new SynchronousQueue<>(),new ConsumerThreadFactory("JOB"),new ThreadPoolExecutor.CallerRunsPolicy());
-    public static void main(String[] args) throws InterruptedException {
-
-    private static final Executor priorityExecutor=new ThreadPoolExecutor(1,5,3, TimeUnit.SECONDS, new PriorityBlockingQueue<>(3),new ConsumerThreadFactory("JOB"),new ThreadPoolExecutor.CallerRunsPolicy());
-        BitSet bitSet=new BitSet(10);
-        bitSet.set(0);
-        bitSet.get(2);
 
     public static void main(String[] args) {
-        System.out.println(bitSet.cardinality());
-
-        ArrayList<Thread> threads=new ArrayList<>();
-        try{
-            Future<String> future= exchangeExecutor.submit(()->{
-                Thread.sleep(200);
-                return "AA";
-            });
-          String str= future.get(100,TimeUnit.MILLISECONDS);
-          System.out.println(str);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+//        System.out.println(bitSet.cardinality());
+//
+//        ArrayList<Thread> threads=new ArrayList<>();
+//        try{
+//            Future<String> future= exchangeExecutor.submit(()->{
+//                Thread.sleep(200);
+//                return "AA";
+//            });
+//          String str= future.get(100,TimeUnit.MILLISECONDS);
+//          System.out.println(str);
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
 
 /*
         for (int i = 0; i < 100; i++) {
@@ -47,10 +40,7 @@ public class ThreadExchange {
         System.out.println("<UNK>");
 */
 
-        for (int i = 0; i < 100; i++) {
-            int finalI = i;
-            priorityExecutor.execute(new ConsumerFunc(finalI));
-        }*/
+
 /*        Thread thread=new Thread(()->{
             Integer i=0;
             while (i++<10){
