f176d2cfa9abbce4c6e77128ad57e96f943994f5
huaifang.yan <huaifang.yan@ly.com>
2025-06-02 21:02:45 +0800

    lua 脚本限制流量 锁的实现

---
+++ spring/src/main/java/com/huaifang/yan/User.java
@@ -0,0 +1,20 @@
+package com.huaifang.yan;
+
+public class User {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
